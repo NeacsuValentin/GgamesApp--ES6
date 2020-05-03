48721845cb228d2d30808e6cd6e642e0628a2656
ValentinNeacsu <neacsu_valentin_iulian@yahoo.ro>
2020-05-03 10:12:41 +0300

    1st commit

---
+++ classes/FetchAPI.js
@@ -0,0 +1,79 @@
+class FetchAPI {
+    constructor(serverURL){
+        this.serverURL = serverURL;
+    }
+
+    async getGameList () {
+        const response = await fetch(`${this.serverURL}/games`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        return response.json();
+    }
+
+
+    // async function fetchAsync () {
+    //     const response = await fetch('https://api.github.com');
+    //     return await response.json();
+    //   }
+
+    async deleteGame (gameID) {
+        try {
+            const response = await fetch(`${this.serverURL}/games/${gameID}`, {
+            method: 'DELETE'
+            })
+            const responseMsg = response.text();;
+            return {
+                succes: true,
+                msg: responseMsg
+            }
+        } catch(error) {
+            return {
+                succes: false,
+                msg: error
+            }
+        }
+    }
+
+    async createGameRequest (gameObj) {
+        const response = await fetch(`${this.serverURL}/games`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: gameObj
+        }) 
+        return response.json();
+    }
+ 
+    async editGame (id,gameObj) {
+        const response = await fetch(`${this.serverURL}/games/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type' : "application/x-www-form-urlencoded"
+            },
+            body: gameObj
+        }) 
+        console.log("PUT response ", response);
+        return response.json();
+
+        // .then(response => {
+        //     console.log("PUT response ", response);
+            
+        //     return response.json()
+        // })
+    }
+
+    async reloadDB () {
+        const response = await fetch(`${this.serverURL}/regenerate-games`, {
+            method: 'GET',
+            headers: {
+                'Content-Type' : "application/x-www-form-urlencoded",
+                'Connection' : 'keep-alive'
+            }
+        });
+        return response.json();
+    }
+}
